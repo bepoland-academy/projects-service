dd4601a54b43071f8e429ce59f9c671c7b227200
miles87 <miles-87@o2.pl>
2019-03-13 13:39:53 +0100

    Ready to deploy

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,9 +6,9 @@ insert into PROJECTS_DB.client (client_id, client_guid, client_name)
 values (3, 'a394ee5c-f2bc-45d0-83d8-c92d8c5ba312', 'HISCOX');
 
 insert into PROJECTS_DB.project (project_id, comments, project_guid, project_name, project_rate, department_id)
-values (1, 'first', '7067a443-d7a1-4dde-b38f-76ce5494978c', 'firstPro', 232, 'fistId');
+values (1, 'first', '7067a443-d7a1-4dde-b38f-76ce5494978c', 'firstPro', 232, 'BANKING');
 insert into PROJECTS_DB.project (project_id, comments, project_guid, project_name, project_rate, department_id)
-values (2, 'second', '8b03fb47-3467-43a1-a4d0-b029002b0de9', 'secondPro', 245, 'secondId');
+values (2, 'second', '8b03fb47-3467-43a1-a4d0-b029002b0de9', 'secondPro', 245, 'DIGITAL');
 
 insert into projects_db.client_project (project_id, client_id)
 values (1, 1);
