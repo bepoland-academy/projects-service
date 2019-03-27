99b9963e62083bc5756a729097a5223998ceb69e
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-03-27 13:38:57 +0100

    Changed database data to more real

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,12 +5,159 @@ values (2, 'c0ed05e5-a6d1-488a-9f1c-d9a152ea4617', 'UNIQA');
 insert into PROJECTS_DB.client (client_id, client_guid, client_name)
 values (3, 'a394ee5c-f2bc-45d0-83d8-c92d8c5ba312', 'HISCOX');
 
-insert into PROJECTS_DB.project (project_id, comments, project_guid, project_name, project_rate, department_guid,
-                                 client_id, active)
-values (1, 'first', '7067a443-d7a1-4dde-b38f-76ce5494978c', 'firstPro', 232, '14708cec-0315-415a-a0d3-17abe1d5df53', 1,
-        true);
-insert into PROJECTS_DB.project (project_id, comments, project_guid, project_name, project_rate, department_guid,
-                                 client_id, active)
-values (2, 'second', '8b03fb47-3467-43a1-a4d0-b029002b0de9', 'secondPro', 245, '77065bda-b226-4b39-9092-94f48aaaa042',
-        2, false);
 
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+
+
+values (1,
+        'Curabitur sit amet scelerisque risus, sollicitudin egestas urna. Curabitur nec sem non metus hendrerit tincidunt. Nam non diam sed est consequat blandit. Etiam nisl eros, porta sit amet pharetra nec, placerat id mi. Fusce pulvinar sit amet justo eget sodales. Praesent felis sem, pulvinar vel est vel, ornare vulputate nulla.',
+        '88a9a983-8337-4462-9383-faf747c24b46',
+        'Refactoring Financial Department Remote System.',
+        100,
+        '14708cec-0315-415a-a0d3-17abe1d5df53',
+        1,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (2,
+        'Vestibulum pulvinar, quam in molestie sodales, velit erat faucibus odio, sit amet porta elit lacus in massa. Pellentesque vitae elit.',
+        '954928d4-b2d5-4e30-ae39-d2516c57ce2e',
+        'Build New Policy Checker.',
+        250,
+        '77065bda-b226-4b39-9092-94f48aaaa042',
+        1,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (3,
+        'Proin facilisis dolor.',
+        '585c5ddf-63f4-480e-8c6c-1a3eb91f9e66',
+        'CRUD For New Customers.',
+        75,
+        'd31182ae-6dca-4153-83b1-fe949e5aade1',
+        1,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (4,
+        'Praesent imperdiet ipsum a ultrices vestibulum. Praesent efficitur ullamcorper dignissim. Cras turpis ligula, ultrices ut posuere commodo, aliquet eget justo. Donec ligula odio.',
+        '7f7f01aa-f49b-465a-80d4-05890de1a74c',
+        'Financial Analysis Of Company Credits.',
+        350,
+        '14708cec-0315-415a-a0d3-17abe1d5df53',
+        2,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (5,
+        'uisque ut vestibulum quam. Quisque elementum lorem in dui venenatis, sit amet imperdiet odio congue. Quisque interdum at diam sodales tempor. Phasellus pellentesque.',
+        'f69b330a-f184-44ea-8849-8006af8f1089',
+        'Implementation of Backbase Forms System.',
+        120,
+        '77065bda-b226-4b39-9092-94f48aaaa042',
+        2,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (6,
+        '',
+        '5d13910d-c6c7-40b3-9e15-507328206939',
+        'Loans Calculator Implementation.',
+        150,
+        'd31182ae-6dca-4153-83b1-fe949e5aade1',
+        2,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+
+values (7,
+         'Pellentesque euismod interdum massa, at lacinia dui cursus rutrum. In nisl tellus, bibendum eget massa a, ultricies lacinia ligula. Mauris a ligula dui. Pellentesque egestas justo eu massa fringilla molestie. Proin congue iaculis aliquet. Fusce congue orci orci. Curabitur turpis.',
+         'a3049ab3-7c87-40d8-9624-6e998684f773',
+         'New Insurances Analysis.',
+         350,
+         'd31182ae-6dca-4153-83b1-fe949e5aade1',
+         3,
+         true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (8,
+        'Vivamus in mauris lacinia, imperdiet orci id, gravida erat. Aenean bibendum, urna in sagittis commodo, nunc orci porttitor orci, ac iaculis justo purus.',
+        'a4a0aad6-4f79-4543-903e-347c9edd7b96',
+        'Refactoring CAR-AND-HOME Policy Manager.',
+        160,
+        '77065bda-b226-4b39-9092-94f48aaaa042',
+        3,
+        true);
+
+insert into PROJECTS_DB.project (project_id,
+                                 comments,
+                                 project_guid,
+                                 project_name,
+                                 project_rate,
+                                 department_guid,
+                                 client_id,
+                                 active)
+values (9,
+        '',
+        '666536ba-906a-4fc3-85bf-e68e3e88c28c',
+        'SALESFORCE Courses for junior developers.',
+        500,
+        'd31182ae-6dca-4153-83b1-fe949e5aade1',
+        3,
+        true);
